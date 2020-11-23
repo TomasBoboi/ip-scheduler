e630dc7a9f5d05f4442bed9ae9af01cc04a740b5
TomasBoboi <boboitomas@gmail.com>
2020-11-23 20:36:45 +0200

    Removed unnecessary files

--- out/gcc-release/ipsched/src/Sink.o.d
+++
@@ -1,7 +0,0 @@
-out/gcc-release//ipsched/src/Sink.o: ipsched/src/Sink.cc \
- ipsched/src/Sink.h \
- /home/tomiticus/OMNeT++/omnetpp-5.6.2/include/omnetpp.h
-
-ipsched/src/Sink.h:
-
-/home/tomiticus/OMNeT++/omnetpp-5.6.2/include/omnetpp.h:
