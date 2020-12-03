b4c79a0bfbc82259abf72122dfcffa1348a27c79
TomasBoboi <boboitomas@gmail.com>
2020-12-03 20:56:24 +0200

    Added WRR scheduler implementation

--- ipsched/scheduler/WRRScheduler.cc
+++ ipsched/scheduler/WRRScheduler.cc
@@ -4,18 +4,20 @@
 
 Define_Module(WRRScheduler);
 
-WRRScheduler::WRRScheduler()
-{
+WRRScheduler::WRRScheduler() {
     readyToScheduleMessage = nullptr;
 }
 
-WRRScheduler::~WRRScheduler()
-{
+WRRScheduler::~WRRScheduler() {
     cancelAndDelete(readyToScheduleMessage);
 }
 
-void WRRScheduler::initialize()
-{
+void WRRScheduler::parseWeights(const char *weightsAsString) {
+    sscanf(weightsAsString, "%d %d %d %d", (userWeights), (userWeights + 1),
+            (userWeights + 2), (userWeights + 3));
+}
+
+void WRRScheduler::initialize() {
     lastServed_nrtLp = simTime();
     lastServed_nrtHp = simTime();
     lastServed_rtLp = simTime();
@@ -26,78 +28,71 @@ void WRRScheduler::initialize()
     scheduleAt(simTime(), readyToScheduleMessage);
 }
 
-void WRRScheduler::handleMessage(cMessage *msg)
-{
-    if (msg == readyToScheduleMessage)
-    {
-        simtime_t firstTime = getMinimumTime(lastServed_nrtLp, lastServed_nrtHp,
-                                             lastServed_rtLp, lastServed_rtHp);
+void WRRScheduler::handleMessage(cMessage *msg) {
+    if (msg == readyToScheduleMessage) {
         bool sent = false;
 
-        if (firstTime == lastServed_nrtLp)
-        {
-            if (getQueueLength("nrtLpQueue") > 0)
-            {
+        simtime_t lastServedWeightedDelta_nrtLp = (simTime() - lastServed_nrtLp)
+                * userWeights[0];
+        simtime_t lastServedWeightedDelta_nrtHp = (simTime() - lastServed_nrtHp)
+                * userWeights[1];
+        simtime_t lastServedWeightedDelta_rtLp = (simTime() - lastServed_rtLp)
+                * userWeights[2];
+        simtime_t lastServedWeightedDelta_rtHp = (simTime() - lastServed_rtHp)
+                * userWeights[3];
+
+        simtime_t chosenTime = getMaximumTime(lastServedWeightedDelta_nrtLp,
+                lastServedWeightedDelta_nrtHp, lastServedWeightedDelta_rtLp,
+                lastServedWeightedDelta_rtHp);
+
+        if (chosenTime == lastServedWeightedDelta_nrtLp) {
+            if (getQueueLength("nrtLpQueue") > 0) {
                 send(new cMessage("schedulerMessage"), "nrtLpQueueControl_out");
                 sent = true;
             }
             lastServed_nrtLp = simTime();
-        }
-        else if (firstTime == lastServed_nrtHp)
-        {
-            if (getQueueLength("nrtHpQueue") > 0)
-            {
+        } else if (chosenTime == lastServedWeightedDelta_nrtHp) {
+            if (getQueueLength("nrtHpQueue") > 0) {
                 send(new cMessage("schedulerMessage"), "nrtHpQueueControl_out");
                 sent = true;
             }
             lastServed_nrtHp = simTime();
-        }
-        else if (firstTime == lastServed_rtLp)
-        {
-            if (getQueueLength("rtLpQueue") > 0)
-            {
+        } else if (chosenTime == lastServedWeightedDelta_rtLp) {
+            if (getQueueLength("rtLpQueue") > 0) {
                 send(new cMessage("schedulerMessage"), "rtLpQueueControl_out");
                 sent = true;
             }
             lastServed_rtLp = simTime();
-        }
-        else if (firstTime == lastServed_rtHp)
-        {
-            if (getQueueLength("rtHpQueue") > 0)
-            {
+        } else if (chosenTime == lastServedWeightedDelta_rtHp) {
+            if (getQueueLength("rtHpQueue") > 0) {
                 send(new cMessage("schedulerMessage"), "rtHpQueueControl_out");
                 sent = true;
             }
             lastServed_rtHp = simTime();
         }
 
-        if (!sent)
-        {
+        if (!sent) {
             scheduleAt(simTime(), readyToScheduleMessage);
         }
-    }
-    else
-    {
+    } else {
         send(msg, "out");
 
         scheduleAt(
-            gate("out")->getTransmissionChannel()->getTransmissionFinishTime(),
-            readyToScheduleMessage);
+                gate("out")->getTransmissionChannel()->getTransmissionFinishTime(),
+                readyToScheduleMessage);
     }
 }
 
-simtime_t WRRScheduler::getMinimumTime(simtime_t time1, simtime_t time2,
-                                              simtime_t time3, simtime_t time4)
-{
-    return std::min(std::min(time1, time2), std::min(time3, time4));
+simtime_t WRRScheduler::getMaximumTime(simtime_t time1, simtime_t time2,
+        simtime_t time3, simtime_t time4) {
+    return std::max(std::max(time1, time2), std::max(time3, time4));
 }
 
-int WRRScheduler::getQueueLength(const char *queueName)
-{
+int WRRScheduler::getQueueLength(const char *queueName) {
     int queueLength;
 
-    Queue *queue = check_and_cast<Queue *>(
-        getParentModule()->getSubmodule(queueName));
+    Queue *queue = check_and_cast<Queue*>(
+            getParentModule()->getSubmodule(queueName));
     queueLength = queue->getQueueLength();
 
     EV << queue->getName() << " length: " << queueLength << "\n";
--- ipsched/scheduler/WRRScheduler.h
+++ ipsched/scheduler/WRRScheduler.h
@@ -16,8 +16,9 @@ protected:
     virtual void handleMessage(cMessage *msg);
 
     virtual int getQueueLength(const char *name);
-    simtime_t getMinimumTime(simtime_t time1, simtime_t time2, simtime_t time3,
+    virtual simtime_t getMaximumTime(simtime_t time1, simtime_t time2, simtime_t time3,
                              simtime_t time4);
+    virtual void parseWeights(const char *weightsAsString);
 
 private:
     cMessage *readyToScheduleMessage;
@@ -26,6 +27,8 @@ private:
     simtime_t lastServed_nrtHp;
     simtime_t lastServed_rtLp;
     simtime_t lastServed_rtHp;
+
+    int userWeights[4];
 };
 
 #endif
